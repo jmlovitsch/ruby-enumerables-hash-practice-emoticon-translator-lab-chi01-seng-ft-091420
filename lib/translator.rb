74e5efcf767206dc8220241edd29eb46cfb2603a
John Lovitsch <jmlovitsch@gmail.com>
2020-09-09 14:24:16 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -34,6 +34,7 @@ def get_japanese_emoticon(file_path = './lib/emoticons.yml', english_emoticon)
         return library[emotion][:japanese]
       end
     end
-    return sorry_message
   end
+  return sorry_message
+
 end
