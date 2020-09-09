c5269889d200c06348a5d7cac648d8ed7b488da6
John Lovitsch <jmlovitsch@gmail.com>
2020-09-09 14:05:14 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -32,8 +32,7 @@ def get_japanese_emoticon(file_path = './lib/emoticons.yml', english_emoticon)
   library.each do |emotion, emoticon_hash|
     emoticon_hash.each do |language, emoji|
       if emoji == english_emoticon
-        binding.pry
-        return :japanese[emoji]
+        return 
       end
     end
   end
